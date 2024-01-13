803f40a07427f80652fd66387b8a14943ecc3248
syg0203 <dudrmsek@gmail.com>
2024-01-13 16:36:11 +0900

    :zap:코드 구조 개선

--- dash/app.py
+++ dash/app.py
@@ -1,7 +1,14 @@
+"""
+app.py: 메인 Dash 애플리케이션 스크립트.
+
+이 스크립트는 Dash 애플리케이션을 초기화하고, 위에서 정의된 plots.py 및 dashboard_layout.py 모듈을 사용하여
+애플리케이션의 레이아웃과 기능을 구성합니다. 이 스크립트는 애플리케이션을 실행하는 데 사용됩니다.
+"""
+
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.wsgi import WSGIMiddleware
-from dashapp import create_dash_app
+from dash.src.dashapp import create_dash_app
 
 app = FastAPI()
 
